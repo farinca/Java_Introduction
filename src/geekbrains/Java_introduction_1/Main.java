8f86bb4b8d7890c0ec2518211324230af724ce30
farinca <arifa@bk.ru>
2020-04-12 18:18:22 +0300

    2

--- src/geekbrains/Java_introduction_1/Main.java
+++ src/geekbrains/Java_introduction_1/Main.java
@@ -1,6 +1,26 @@
 package geekbrains.Java_introduction_1;
-
 public class Main {
+    //ex1 (1 задание)
+    public static void main (String[] args) {
 
+    }
+    // 2 задание
+    public static void ex2(){
+        byte a = 6;
+        int b = 12000;
+        long c = -280L;
+        float d = 15.25f;
+        double e = -42.425;
+        char f = '@';
+        boolean g = false;
+    }
+    // 3 задание
+    public static float ex3 (float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
+    public static void ex4(int a, int b){
+        boolean bool = (10<=(a+b)<=20);
+        if (bool)
+        System.out.prinln(bool);
     }
 }

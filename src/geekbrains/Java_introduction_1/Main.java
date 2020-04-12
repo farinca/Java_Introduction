2fb09a3ed6ed05fe65b1a6c2d13cc519e1e5419f
farinca <arifa@bk.ru>
2020-04-12 20:31:41 +0300

    End

--- src/geekbrains/Java_introduction_1/Main.java
+++ src/geekbrains/Java_introduction_1/Main.java
@@ -1,11 +1,13 @@
 package geekbrains.Java_introduction_1;
+
 public class Main {
-    //ex1 (1 задание)
-    public static void main (String[] args) {
+    //ex1 (1 задание) Метод main
+    public static void main(String[] args) {
 
     }
-    // 2 задание
-    public static void ex2(){
+
+    // 2 задание объявить и инициализировать переменные
+    public static void variables() {
         byte a = 6;
         int b = 12000;
         long c = -280L;
@@ -14,13 +16,43 @@ public class Main {
         char f = '@';
         boolean g = false;
     }
+
     // 3 задание
-    public static float ex3 (float a, float b, float c, float d) {
+    // Метод возвращяющий результат (аргументы типа float)
+    public static float calculate(float a, float b, float c, float d) {
         return a * (b + (c / d));
     }
-    public static void ex4(int a, int b){
-        boolean bool = (10<=(a+b)<=20);
-        if (bool)
-        System.out.prinln(bool);
+
+    // 4 задание
+    // Написать метод, принимающий на вход два целых числа и проверяющий,
+    // что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true,
+    // в противном случае – false.
+    public static boolean compare(int a, int b) {
+        return ((10 <= (a+b)) && ((a+b) <= 20));
+    }
+
+    //5 задание
+    //Написать метод, которому в качестве параметра передается целое число,
+    //метод должен напечатать в консоль, положительное ли число передали или отрицательное.
+    //Замечание: ноль считаем положительным числом.
+    public static void printPositiveNegative(int a) {
+        if (a>0){
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+
+    }
+    //6 задание
+    //Написать метод, которому в качестве параметра передается целое число.
+    //Метод должен вернуть true, если число отрицательное.
+    public static boolean ifNegative(int a){
+        return (a<0);
+    }
+    //7 задание
+    //Написать метод, которому в качестве параметра передается строка, обозначающая имя.
+    //Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
+    public static void printHelloName(String a){
+        System.out.println("Привет, "+ a+"!");
     }
 }
